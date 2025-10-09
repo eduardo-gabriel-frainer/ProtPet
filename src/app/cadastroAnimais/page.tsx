286f57a7f7428a3d2c667133453445b26db25ed5
Eduardo Gabriel Frainer <eduardogfrainer@gmail.com>
2025-10-09 20:48:48 -0300

    comeco da tela de cadasrto

--- src/app/cadastroAnimais/page.tsx
+++ src/app/cadastroAnimais/page.tsx
@@ -4,7 +4,11 @@ export default function cadastroAnimais() {
     return(
         <div>
             <div className="min-h-screen">
-                Cadastro
+                <h1 className="text-[#FF862F] text-5xl font-bold">Formulário de <br/> animais perdidos ou <br/> encontrados</h1>
+
+                <h2 className="pt-8 text-1xl">Ajude a reunir um animal com sua familia. Preencha <br/>
+                este formulário para divulgar o animal que você perdeu <br/> ou encontrou.
+                </h2>
             </div>
         <Footer/>
         </div>
