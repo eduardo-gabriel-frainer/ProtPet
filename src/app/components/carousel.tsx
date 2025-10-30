ec889c909fe4679e0b0e6c53dffcc0f4b1a78846
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-10-30 20:27:45 -0300

    arrumando botoes e pagina principal

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,96 +1,100 @@
-"use client";
-import { useState, useEffect } from "react";
-import Card from "./card";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+  "use client";
+  import { useState, useEffect } from "react";
+  import Card from "./card";
+  import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-export default function Carousel() {
-  const pets = [
-    { id: 1, especie: "Gato", status: "Perdido", image: "/imagesCarousel/1.png" },
-    { id: 2, especie: "Cachorro", status: "Encontrado", image: "/imagesCarousel/2.png" },
-    { id: 3, especie: "Calopsita", status: "Encontrado", image: "/imagesCarousel/3.png" },
-    { id: 4, especie: "Gato", status: "Perdido", image: "/imagesCarousel/4.png" },
-    { id: 5, especie: "Cabra", status: "Perdido", image: "/imagesCarousel/5.png" },
-    { id: 6, especie: "Gato", status: "Perdido", image: "/imagesCarousel/6.png" },
-    { id: 7, especie: "Cabra", status: "Perdido", image: "/imagesCarousel/7.png" },
-    { id: 8, especie: "", status: "Ver mais!!!", image: "/imagesCarousel/vermais.png" },
-  ];
+  export default function Carousel() {
+    const pets = [
+      { id: 1, especie: "Gato", status: "Perdido", image: "/imagesCarousel/1.png" },
+      { id: 2, especie: "Cachorro", status: "Encontrado", image: "/imagesCarousel/2.png" },
+      { id: 3, especie: "Calopsita", status: "Encontrado", image: "/imagesCarousel/3.png" },
+      { id: 4, especie: "Gato", status: "Perdido", image: "/imagesCarousel/4.png" },
+      { id: 5, especie: "Cabra", status: "Perdido", image: "/imagesCarousel/5.png" },
+      { id: 6, especie: "Gato", status: "Perdido", image: "/imagesCarousel/6.png" },
+      { id: 7, especie: "Cabra", status: "Perdido", image: "/imagesCarousel/7.png" },
+      { id: 8, especie: "", status: "Ver mais!!!", image: "/imagesCarousel/vermais.png" },
+    ];
 
-  const [index, setIndex] = useState(0);
-  const [cardsVisiveis, setCardsVisiveis] = useState(4);
-  const [cardWidth] = useState(325);
-  const [ultimo, setUltimo] = useState(false);
-  const [primeiro, setPrimeiro] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [cardsVisiveis, setCardsVisiveis] = useState(4);
+    const [cardWidth] = useState(325);
+    const [ultimo, setUltimo] = useState(false);
+    const [primeiro, setPrimeiro] = useState(true);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 1000) {
-        setCardsVisiveis(1)
-      } else if (window.innerWidth < 1400) {
-        setCardsVisiveis(2);
-      } else if (window.innerWidth < 1780) {
-        setCardsVisiveis(3);
-      } else {
-        setCardsVisiveis(4);
+    useEffect(() => {
+      const handleResize = () => {
+        if (window.innerWidth < 1000) {
+          setCardsVisiveis(1)
+        } else if (window.innerWidth < 1400) {
+          setCardsVisiveis(2);
+        } else if (window.innerWidth < 1780) {
+          setCardsVisiveis(3);
+        } else {
+          setCardsVisiveis(4);
+        }
+      };
+
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+      if (index == 0){
+        setPrimeiro(true);
       }
+
+      if (index >= pets.length - cardsVisiveis){
+        setUltimo(true);
+      }
+
+    }, [index]);
+
+    const avancar = () => {
+      if (index < pets.length - cardsVisiveis) {
+        setIndex(index + 1);
+      } 
+      setPrimeiro(false);
     };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    const voltar = () => {
+      if (index > 0) {
+          setIndex(index - 1)
+      }
 
-  const avancar = () => {
-    if (index < pets.length - cardsVisiveis) {
-      setIndex(index + 1);
-    } 
-
-    if (index == 3){
-      setUltimo(true);
+      setUltimo(false)
     }
-    setPrimeiro(false);
-  };
 
-  const voltar = () => {
-    if (index > 0) {
-        setIndex(index - 1)
-    }
-   
-    if (index == 1){
-      setPrimeiro(true);
-    }
-    setUltimo(false)
-  }
+    return (
+      <div className="relative flex items-center justify-center mt-10">
+        {/* Botão esquerda */}
+        <button className={`absolute left-30 bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900 ${primeiro ? 'hidden' : ''}`}
+        onClick={voltar}>
+          <FaArrowLeft size={20}/>
+        </button>
 
-  return (
-    <div className="relative flex items-center justify-center mt-10">
-      {/* Botão esquerda */}
-      <button className={`absolute left-30 bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900 ${primeiro ? 'hidden' : ''}`}
-      onClick={voltar}>
-        <FaArrowLeft size={20}/>
-      </button>
-
-      {/* Área dos cards */}
-      <div
-        className="overflow-hidden transition-all duration-300 pl-6"
-        style={{
-          width: `${cardsVisiveis * cardWidth}px`,
-        }}
-      >
-        <div    
-          className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${index * 330}px)` }}
+        {/* Área dos cards */}
+        <div
+          className="overflow-hidden transition-all duration-300 pl-6"
+          style={{
+            width: `${cardsVisiveis * cardWidth}px`,
+          }}
         >
-           {pets.map((pet) => (
-            <Card key={pet.id} pet={pet} />
-          ))}
+          <div    
+            className="flex transition-transform duration-500"
+            style={{ transform: `translateX(-${index * 330}px)` }}
+          >
+            {pets.map((pet) => (
+              <Card key={pet.id} pet={pet} />
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* Botão direita */}
-      <button className={`absolute right-30  ${ultimo ? "hidden" : ""} bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900`}
-      onClick={avancar}>
-          <FaArrowRight size={20}/>
-      </button>
-    </div>
-  );
-}
+        {/* Botão direita */}
+        <button className={`absolute right-30  ${ultimo ? "hidden" : ""} bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900`}
+        onClick={avancar}>
+            <FaArrowRight size={20}/>
+        </button>
+      </div>
+    );
+  }
--- src/app/components/secaoIntro.tsx
+++ src/app/components/secaoIntro.tsx
@@ -3,12 +3,16 @@ export default function secaoIntro() {
     <div className="min-h-screen bg-[#FF862F] flex items-center justify-between px-40 -mt-5">
         <div className="text-white text-8xl align-items font-semibold -mt-5">
           <img src="/osso.png" className="w-40 h-auto hover:scale-110 duration-300" />
-          <div>" A voz que <br />protege quem <br />não pode falar. "</div>
-        </div>
+          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl">
+            " A voz que <br />
+            protege quem <br />
+            não pode falar. "
+          </div>
+          </div>
 
-        <div className="text-white text-6xl align-items">
-           <img src="/cachorroTelaPrincipal.png" className="w-160 h-auto -mt-20" />
-        </div>
+          <div className="text-white text-6xl align-items">
+            <img src="/cachorroTelaPrincipal.png" className="w-60 sm:w-96 md:w-120 lg:w-160 h-auto -mt-10 md:-mt-20" />
+          </div>
     </div>  
     )
 }
