52c078bff4ad076a3658cbaa1aedabe459b1b063
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-10-30 19:55:39 -0300

    arrumando as setas

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -17,7 +17,9 @@ export default function Carousel() {
 
   const [index, setIndex] = useState(0);
   const [cardsVisiveis, setCardsVisiveis] = useState(4);
-  const [cardWidth] = useState(320);
+  const [cardWidth] = useState(325);
+  const [ultimo, setUltimo] = useState(false);
+  const [primeiro, setPrimeiro] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,33 +42,43 @@ export default function Carousel() {
   const avancar = () => {
     if (index < pets.length - cardsVisiveis) {
       setIndex(index + 1);
+    } 
+
+    if (index == 3){
+      setUltimo(true);
     }
+    setPrimeiro(false);
   };
 
   const voltar = () => {
     if (index > 0) {
         setIndex(index - 1)
     }
+   
+    if (index == 1){
+      setPrimeiro(true);
+    }
+    setUltimo(false)
   }
 
   return (
     <div className="relative flex items-center justify-center mt-10">
       {/* Botão esquerda */}
-      <button className="absolute left-30 bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900"
+      <button className={`absolute left-30 bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900 ${primeiro ? 'hidden' : ''}`}
       onClick={voltar}>
         <FaArrowLeft size={20}/>
       </button>
 
       {/* Área dos cards */}
       <div
-        className="overflow-hidden transition-all duration-300"
+        className="overflow-hidden transition-all duration-300 pl-6"
         style={{
           width: `${cardsVisiveis * cardWidth}px`,
         }}
       >
         <div    
           className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${index * 335}px)` }}
+          style={{ transform: `translateX(-${index * 330}px)` }}
         >
            {pets.map((pet) => (
             <Card key={pet.id} pet={pet} />
@@ -75,7 +87,7 @@ export default function Carousel() {
       </div>
 
       {/* Botão direita */}
-      <button className="absolute right-30 bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900"
+      <button className={`absolute right-30  ${ultimo ? "hidden" : ""} bg-[#FF862F] text-white rounded-full hover:bg-[#ff730e] p-3 text-3xl z-900`}
       onClick={avancar}>
           <FaArrowRight size={20}/>
       </button>
