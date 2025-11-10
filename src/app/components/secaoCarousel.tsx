c5fafca5310962e4797e252d057a9ca88e581f94
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-11-10 20:48:32 -0300

    feat: implementar card ong

--- src/app/components/secaoCarousel.tsx
+++ src/app/components/secaoCarousel.tsx
@@ -4,7 +4,7 @@ export default function secaoIntro() {
     return(
       <div className="min-h-screen bg-[#fff4e5]">
         <div className="text-[#FF862F] text-6xl text-center pt-10 pb-15 font-semibold">
-          Animais Perdidos ou Encontrados
+          Animais Perdidos, Encontrados ou para adoção
         </div>
          <Carousel />
       </div>
